910cc4217de087c7092198a89a490d73ef6f6519
DaironJimenez <55242140+DaironJimenez@users.noreply.github.com>
2020-05-04 14:58:06 -0400

    setup reduxform

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,27 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
-import reducers from "./reducers";
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
-import "./style/main.scss";
+import './style/main.scss';
 
-import Layout from "./components/layout";
+import history from './history';
+
+import Layout from './components/layout';
+import Signin from './components/auth/signin';
+import Signup from './components/auth/signup';
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
+      <Router history={history}>
         <Layout>
-          
-        </Layout>
-      </BrowserRouter>
-    </Provider>,
-    document.querySelector(".app-wrapper")
-  );
-}
 
-document.addEventListener("DOMContentLoaded", main);
+            <Switch>
+              <Route path='/' exact component={Signin}/>
+              <Route path='/signin' exact component={Signin}/>
+              <Route path='/signup' exact component={Signup}/>
+            </Switch>
+        </Layout>
+      
+      </Router>
+      </BrowserRouter>
+    </Provider>
+    , document.querySelector('.app-wrapper'));
+}
