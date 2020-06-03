e827f6281101786b7323179d22474f71ed941f71
DaironJimenez <55242140+DaironJimenez@users.noreply.github.com>
2020-06-03 13:45:49 -0400

    setup purchase history child components and corresponding styles

---
+++ src/components/account/purchaseDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PurchaseDetail extends Component {
+    render() {
+        const { className } = this.props;
+
+        <div className={`${className} PurchaseDetail`}>
+            Purchase detail goes here
+        </div>
+    }
+}
+
+export default PurchaseDetail;
---
+++ src/components/account/purchases.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Purchases extends Component {
+    render() {
+        const { className } = this.props;
+
+        <div className={`${className} purchases`}>
+            purchases go here
+        </div>
+    }
+}
+
+export default Purchases;
